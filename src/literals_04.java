c1df1fe2b85ae5de49be8d121be620e96d8b67ff
AtanuBag001 <bagatanu079@gmail.com>
2026-03-02 19:20:22 +0530

    Initial commit

---
+++ src/literals_04.java
@@ -0,0 +1,5 @@
+public class literals_04 {
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+}
---
+++ src/sumOfThree.java
@@ -0,0 +1,10 @@
+public class sumOfThree {
+   public static void main(String[] args) {
+        System.out.print("Sum of these number is ");
+        int num1 = 6;
+        int num2 = 7;
+        int num3 = 8;
+        int sum = num1 + num2 + num3 ;
+        System.out.println(sum);
+    }
+}
